437b1b5185832536611781a805be301544d5d599
ChromicQuanta <59491666+ChromicQuanta@users.noreply.github.com>
2024-03-12 12:22:56 +1100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ WebSocket = require("ws")
 http = require("http")
 http.createServer((q,s)=>{
     s.end("GOWOT")
-}).listen(80)
+}).listen(1234)
 function createWS(){
 ws = new WebSocket("wss://ourworldoftext.com/cq/ws/?key=4q6x")
     
